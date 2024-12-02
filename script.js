35a5f49f054542c4b7891e1bc911a3ba889deda5
Matthewmamangon <matthew.mamangon@pcsfi.edu.ph>
2024-12-03 02:04:36 +0800

    ...

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const buttons = document. quarySelectorforAll("[data-carousel-button]")
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
 buttons.forEach(button => {
-    button.addEventListener("click", ()=> {
-        const offest = button.dataset.carouselButton === "next" ? 1 : -1 
+    buttons.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1 
         const slides = button 
         .closest("[data-carousel]")
         .querySelector("[data-slides]")
 
-        const activeSlide = slides.querySelectors("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) = offest
+        const activeSlides = slides.querySelectors("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlides) = offset
         if (newIndex < 0) newIndex = slides.children.length -1 
-        if ( newIndex >= slides.children.length) newIndex = 0 
+        if ( newIndex >= activeSlides.children.length) newIndex = 0 
 
         slides.children[newIndex].dataset.active = true 
-        delete activeSlide.dataset.active
+        delete activeSlides.dataset.active
     })
 }) 
